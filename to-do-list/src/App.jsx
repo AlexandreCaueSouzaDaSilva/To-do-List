147f500bb8b470cbf0d57fad3a3d4a51b911b1c7
AlexandreCaueSouzadaSilva <caue8766@gmail.com>
2026-05-03 00:55:51 -0300

    Front conectado com back, adiciona e visualiza tarefa, funcionando com JSON

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,7 +1,38 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './css/App.css' 
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  async function getTasks() {
+    const response = await fetch("http://localhost:3000/tarefas"); // Fazendo uma requisição GET para obter as tarefas do backend
+    const data = await response.json(); // Convertendo a resposta para JSON
+    setTasks(data);
+  }
+
+  async function addTask() {
+    await fetch("http://localhost:3000/tarefas", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        titulo: task
+      })
+    });
+
+    setTask("");
+    getTasks();
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+
 //* To Do List
   return(
 <div className="App">
@@ -9,8 +40,13 @@ function App() {
 
   <div className="input-container">
     <div className="agrupado-inputs">
-       <input type="text" placeholder="Adicione uma tarefa" />
-       <button>Adicionar</button>
+       <input 
+       type="text" 
+       placeholder="Adicione uma tarefa" 
+       value={task}
+       onChange={(e) => setTask(e.target.value)}
+       />
+       <button onClick={addTask}>Adicionar</button>
     </div>
 
 
@@ -29,19 +65,20 @@ function App() {
 
 
     <table border={1}>
-      <tr>
-        <td><input type="checkbox" /> Tarefa 1</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" /> Tarefa 2</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" /> Tarefa 3</td>
-      </tr>
+      <tbody>
+        {tasks.map((item) => (
+          <tr key={item.id}>
+            <td>
+              <input type="checkbox" /> {item.titulo}
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   </div>
 
 </div>
  )
 }
+
 export default App
